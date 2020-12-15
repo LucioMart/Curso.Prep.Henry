ca8d2811bcad8b62f7d423a1dce492b5bc1b78c9
LucioMart <luchoweb15@gmail.com>
2020-12-15 20:29:33 -0300

    Ejercicios resultos de JSV

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -8,6 +8,18 @@ function crearUsuario() {
   // {{nombre}} debe ser el nombre definido en cada instancia
   // Devuelve la clase
   // Tu código:
+  function Usuario(opciones) {
+    this.usuario = opciones.usuario;
+    this.nombre = opciones.nombre;
+    this.email = opciones.email;
+    this.password = opciones.password;
+  }
+
+  Usuario.prototype.saludar = function() {
+    return 'Hola, mi nombre es ' + this.nombre;
+  }
+
+  return Usuario;
 
 }
 
@@ -15,6 +27,9 @@ function agregarMetodoPrototype(Constructor) {
   // Agrega un método al Constructor del `prototype`
   // El método debe llamarse "saludar" y debe devolver la string "Hello World!"
   // Tu código:
+  Constructor.prototype.saludar = function() {
+    return 'Hello World!';
+  }
 
 }
 
@@ -24,6 +39,18 @@ function agregarStringInvertida() {
   // Ej: 'menem'.reverse() => menem
   // 'toni'.reverse() => 'inot'
   // Pista: Necesitarás usar "this" dentro de "reverse"
+
+  String.prototype.reverse = function() {
+
+    var x = this.length;
+    var cadena = "";
+    while (x>=0) {
+      cadena = cadena + this.charAt(x);
+      x--;
+    }
+    return cadena;
+    
+  };
 
 }
 
